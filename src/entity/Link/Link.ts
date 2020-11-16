1d3dbb8fbb6b34dcff91ecf4fc02dc3474daf40d
Kasey Cantu <kaseypaulcantu@gmail.com>
2020-11-16 14:39:15 -0600

    Exposed the Link model to type-graphql.

--- src/entity/Link/Link.ts
+++ src/entity/Link/Link.ts
@@ -8,28 +8,38 @@ import {
   UpdateDateColumn,
   BeforeInsert
 } from "typeorm";
+import { Field, ID, ObjectType } from "type-graphql";
+
 
 @Entity({ name: "links" })
+@ObjectType()
 export class Link extends BaseEntity {
 
   @PrimaryGeneratedColumn("uuid", { name: "link_id" })
+  @Field(() => ID)
   id!: string;
 
   @Column({ name: "link_title", nullable: false, type: "varchar" })
+  @Field({ nullable: false })
   title!: string;
 
   @Column({ name: "link_url", nullable: false, type: "varchar" })
+  @Field({ nullable: false })
   url!: string;
 
   @Column({ name: "link_description", nullable: false, type: "text" })
-  description?: string;
+  @Field({ nullable: false })
+  description!: string;
 
   @Column({ name: "programming_languages", nullable: true, type: "varchar" })
-  programmingLanguages?: string;
+  @Field({ nullable: false })
+  programmingLanguages!: string;
 
   @CreateDateColumn({ name: "created_at", nullable: false, type: "timestamptz" })
+  @Field(() => Date)
   createdAt!: Date;
 
   @UpdateDateColumn({ name: "updated_at", nullable: false, type: "timestamptz" })
+  @Field(() => Date)
   updatedAt!: Date;
 }
