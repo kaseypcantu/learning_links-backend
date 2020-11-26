fa6ff27ef2e73d9078b1c5b4f4e8cfc8ac6f08b5
kaseypcantu <kaseypaulcantu@gmail.com>
2020-11-26 00:46:53 -0600

    Removed elasticache env vars and added new vars for dev and prod hosts of PostgreSQL and Redis.

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -6,13 +6,12 @@ import { Connection } from "typeorm";
 
 export const PG_USER = process.env.POSTGRES_USER as string;
 export const PG_PASSWORD = process.env.POSTGRES_PASSWORD as string;
-export const PG_HOST = process.env.POSTGRES_HOST as string;
+export const PG_PROD_HOST = process.env.POSTGRES_HOST as string;
+export const PG_DEV_HOST = process.env.POSTGRES_DEV_HOST as string;
 export const PG_DATABASE = process.env.POSTGRES_DB as string;
 export const PG_PORT = Number.parseInt(process.env.POSTGRES_PORT as string) ?? 5432;
-export const ELASTICACHE_HOST = process.env.AWS_ELASTICACHE_HOST as string;
-export const ELASTICACHE_PORT = Number.parseInt(process.env.AWS_ELASTICACHE_PORT as string);
-export const ELASTICACHE_PASSWORD = process.env.AWS_ELASTICACHE_PASSWORD as string;
-export const ELASTICACHE = process.env.AWS_ELASTICACHE as string;
+export const REDIS_PROD_HOST = process.env.REDIS_HOST as string;
+export const REDIS_DEV_HOST = process.env.REDIS_DEV_HOST as string;
 export const LOG_LEVEL = process.env.LOG_LEVEL as string ?? "debug";
 export const isProd = process.env.NODE_ENV === "production";
 
@@ -26,7 +25,7 @@ function logFormatFlag(): string {
 export const LOG_FORMAT = logFormatFlag() ? process.env.LOG_FORMAT as string : "json";
 
 // const criticalValues: object = {
-//   "PG_HOST": PG_HOST,
+//   "PG_PROD_HOST": PG_PROD_HOST,
 //   "PG_USER": PG_USER,
 //   "PG_PASSWORD": PG_PASSWORD,
 //   "PG_DATABASE": PG_DATABASE
@@ -34,7 +33,7 @@ export const LOG_FORMAT = logFormatFlag() ? process.env.LOG_FORMAT as string : "
 
 export const initDB = async (): Promise<void> => {
   await createTypeOrmConnection(
-    isProd ? PG_HOST : 'localhost',
+    isProd ? PG_PROD_HOST : PG_DEV_HOST,
     PG_PORT,
     PG_USER,
     PG_PASSWORD,
